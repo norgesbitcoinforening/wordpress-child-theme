58ec25328f856ca66fdd7e487f2947e5fc099578
Nicholas <sir_nic1@hotmail.com>
2016-09-05 09:42:09 +0200

    Removed console.log

--- js/bitcoinaverage.js
+++ js/bitcoinaverage.js
@@ -5,7 +5,6 @@ $.ajax({
    xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
   }
 }).done(function( data ) {
-	console.log(data);
 	  var m = JSON.parse(data);
  	  m["24h_avg"] = m["24h_avg"].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ");
 	  $("#average-price").append("<a href='https://bitcoinaverage.com#NOK' taget='_Blank'><h1>Kr: "+m["24h_avg"]+",-<h1></a>");
